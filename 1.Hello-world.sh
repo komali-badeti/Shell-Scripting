e0f30f9062476c96c23c7385c10ec25144d83101
komali <komali@gmail.com>
2025-11-22 00:26:19 +0530

    l1

---
+++ 1.Hello-world.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Enter your name"
+read NAME
+echo "welcome $NAME"
+
+
+echo "Enter your first name:"
+read FIRST
+echo "Enter your second name:"
+read SECOND
+echo "full name is $FIRST $SECOND"
--- Hello-world.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-echo "Enter your name"
-read NAME
-echo "welcome $NAME"
